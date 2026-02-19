4894f56f47e1882fa193b0024c3f334fa45dfbed
ave <ave@DESKTOP-OQR929T>
2026-02-19 22:18:09 +0700

    fourth commitblol

--- SDL/src/sdl/MyData.java
+++ SDL/src/sdl/MyData.java
@@ -14,7 +14,7 @@ public class MyData {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-                // perbaikan dari teol
+                // perbaikan dari teoll
             int[] array = {1,2,3,4,5,6,7,8,9,10};
             int total = 0;
             for(int i = 0;i < array.length; i++) {
